d6daf60b82527520789e49620b497703f77d4f6d
Gunpreet Singh <alanwalker7172@gmail.com>
2023-05-10 10:51:43 +0530

    remove permission check in getAllBanners

--- src/routes/banner.route.js
+++ src/routes/banner.route.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 router.post("/createbanner", validateToken, admin, createBanner);
 router.put("/updatebanner", validateToken, admin, updateBanner);
-router.get("/getAllBanners", validateToken, admin, getAllBanners);
+router.get("/getAllBanners", getAllBanners);
 router.get("/getBannerDetails/:id", validateToken, admin, getBannerDetails);
 
 module.exports = router;
